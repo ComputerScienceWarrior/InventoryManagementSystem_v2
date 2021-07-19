c1ce0a2224f16d85ed79070d2354cedbc7283717
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2021-07-19 12:37:27 -0700

    Add an inventory fields groupbox.

--- InventoryManagementSystem_v2/Form1.Designer.cs
+++ InventoryManagementSystem_v2/Form1.Designer.cs
@@ -29,190 +29,264 @@ namespace InventoryManagementSystem_v2
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.panel2 = new System.Windows.Forms.Panel();
-			this.pictureBox1 = new System.Windows.Forms.PictureBox();
-			this.label1 = new System.Windows.Forms.Label();
+			this.UserInfoPanel = new System.Windows.Forms.Panel();
+			this.UserImage = new System.Windows.Forms.PictureBox();
+			this.UsernameLabel = new System.Windows.Forms.Label();
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
-			this.button3 = new System.Windows.Forms.Button();
-			this.button4 = new System.Windows.Forms.Button();
-			this.button5 = new System.Windows.Forms.Button();
-			this.button6 = new System.Windows.Forms.Button();
 			this.label2 = new System.Windows.Forms.Label();
+			this.SearchButton = new System.Windows.Forms.Button();
+			this.DisplayButton = new System.Windows.Forms.Button();
+			this.UpdateButton = new System.Windows.Forms.Button();
+			this.RemoveButton = new System.Windows.Forms.Button();
+			this.AddButton = new System.Windows.Forms.Button();
 			this.panel1 = new System.Windows.Forms.Panel();
-			this.panel2.SuspendLayout();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.ItemName = new System.Windows.Forms.TextBox();
+			this.ItemPrice = new System.Windows.Forms.TextBox();
+			this.ItemQuantity = new System.Windows.Forms.TextBox();
+			this.ItemNameLabel = new System.Windows.Forms.Label();
+			this.ItemPriceLabel = new System.Windows.Forms.Label();
+			this.ItemQuantityLabel = new System.Windows.Forms.Label();
+			this.UserInfoPanel.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.UserImage)).BeginInit();
 			this.groupBox1.SuspendLayout();
 			this.panel1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// panel2
+			// UserInfoPanel
 			// 
-			this.panel2.Controls.Add(this.pictureBox1);
-			this.panel2.Controls.Add(this.label1);
-			this.panel2.Location = new System.Drawing.Point(1018, 1);
-			this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.panel2.Name = "panel2";
-			this.panel2.Size = new System.Drawing.Size(389, 281);
-			this.panel2.TabIndex = 1;
+			this.UserInfoPanel.Controls.Add(this.UserImage);
+			this.UserInfoPanel.Controls.Add(this.UsernameLabel);
+			this.UserInfoPanel.Location = new System.Drawing.Point(909, 12);
+			this.UserInfoPanel.Margin = new System.Windows.Forms.Padding(4);
+			this.UserInfoPanel.Name = "UserInfoPanel";
+			this.UserInfoPanel.Size = new System.Drawing.Size(354, 253);
+			this.UserInfoPanel.TabIndex = 1;
 			// 
-			// pictureBox1
+			// UserImage
 			// 
-			this.pictureBox1.BackgroundImage = global::InventoryManagementSystem_v2.Properties.Resources.baseline_account_circle_black_24dp;
-			this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-			this.pictureBox1.Location = new System.Drawing.Point(113, 42);
-			this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(140, 115);
-			this.pictureBox1.TabIndex = 1;
-			this.pictureBox1.TabStop = false;
+			this.UserImage.BackgroundImage = global::InventoryManagementSystem_v2.Properties.Resources.baseline_account_circle_black_24dp;
+			this.UserImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.UserImage.Location = new System.Drawing.Point(103, 38);
+			this.UserImage.Margin = new System.Windows.Forms.Padding(4);
+			this.UserImage.Name = "UserImage";
+			this.UserImage.Size = new System.Drawing.Size(127, 103);
+			this.UserImage.TabIndex = 1;
+			this.UserImage.TabStop = false;
 			// 
-			// label1
+			// UsernameLabel
 			// 
-			this.label1.AutoSize = true;
-			this.label1.ForeColor = System.Drawing.SystemColors.Control;
-			this.label1.Location = new System.Drawing.Point(95, 174);
-			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(143, 20);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "<Username Here>";
+			this.UsernameLabel.AutoSize = true;
+			this.UsernameLabel.ForeColor = System.Drawing.SystemColors.Control;
+			this.UsernameLabel.Location = new System.Drawing.Point(86, 157);
+			this.UsernameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.UsernameLabel.Name = "UsernameLabel";
+			this.UsernameLabel.Size = new System.Drawing.Size(146, 18);
+			this.UsernameLabel.TabIndex = 0;
+			this.UsernameLabel.Text = "<Username Here>";
 			// 
 			// groupBox1
 			// 
 			this.groupBox1.Controls.Add(this.label2);
-			this.groupBox1.Controls.Add(this.button6);
-			this.groupBox1.Controls.Add(this.button5);
-			this.groupBox1.Controls.Add(this.button4);
-			this.groupBox1.Controls.Add(this.button3);
-			this.groupBox1.Controls.Add(this.button2);
-			this.groupBox1.Controls.Add(this.button1);
-			this.groupBox1.Location = new System.Drawing.Point(4, 374);
-			this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.groupBox1.Controls.Add(this.SearchButton);
+			this.groupBox1.Controls.Add(this.DisplayButton);
+			this.groupBox1.Controls.Add(this.UpdateButton);
+			this.groupBox1.Controls.Add(this.RemoveButton);
+			this.groupBox1.Controls.Add(this.AddButton);
+			this.groupBox1.Location = new System.Drawing.Point(11, 325);
+			this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
 			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.groupBox1.Size = new System.Drawing.Size(381, 278);
+			this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+			this.groupBox1.Size = new System.Drawing.Size(329, 250);
 			this.groupBox1.TabIndex = 0;
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Inventory Options";
-			// 
-			// button1
-			// 
-			this.button1.Location = new System.Drawing.Point(8, 48);
-			this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(364, 29);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
-			// 
-			// button2
-			// 
-			this.button2.Location = new System.Drawing.Point(8, 84);
-			this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(364, 29);
-			this.button2.TabIndex = 1;
-			this.button2.Text = "button2";
-			this.button2.UseVisualStyleBackColor = true;
-			// 
-			// button3
-			// 
-			this.button3.Location = new System.Drawing.Point(8, 120);
-			this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(364, 29);
-			this.button3.TabIndex = 2;
-			this.button3.Text = "button3";
-			this.button3.UseVisualStyleBackColor = true;
-			// 
-			// button4
-			// 
-			this.button4.Location = new System.Drawing.Point(8, 156);
-			this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.button4.Name = "button4";
-			this.button4.Size = new System.Drawing.Size(364, 29);
-			this.button4.TabIndex = 3;
-			this.button4.Text = "button4";
-			this.button4.UseVisualStyleBackColor = true;
-			// 
-			// button5
-			// 
-			this.button5.Location = new System.Drawing.Point(8, 192);
-			this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.button5.Name = "button5";
-			this.button5.Size = new System.Drawing.Size(364, 29);
-			this.button5.TabIndex = 4;
-			this.button5.Text = "button5";
-			this.button5.UseVisualStyleBackColor = true;
-			// 
-			// button6
-			// 
-			this.button6.Location = new System.Drawing.Point(8, 229);
-			this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.button6.Name = "button6";
-			this.button6.Size = new System.Drawing.Size(364, 29);
-			this.button6.TabIndex = 5;
-			this.button6.Text = "button6";
-			this.button6.UseVisualStyleBackColor = true;
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
 			this.label2.ForeColor = System.Drawing.SystemColors.Control;
-			this.label2.Location = new System.Drawing.Point(95, 22);
+			this.label2.Location = new System.Drawing.Point(74, 21);
 			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(189, 20);
+			this.label2.Size = new System.Drawing.Size(181, 18);
 			this.label2.TabIndex = 2;
 			this.label2.Text = "Select an option Below";
 			// 
+			// SearchButton
+			// 
+			this.SearchButton.Location = new System.Drawing.Point(19, 143);
+			this.SearchButton.Margin = new System.Windows.Forms.Padding(4);
+			this.SearchButton.Name = "SearchButton";
+			this.SearchButton.Size = new System.Drawing.Size(288, 26);
+			this.SearchButton.TabIndex = 4;
+			this.SearchButton.Text = "Search";
+			this.SearchButton.UseVisualStyleBackColor = true;
+			// 
+			// DisplayButton
+			// 
+			this.DisplayButton.Location = new System.Drawing.Point(19, 202);
+			this.DisplayButton.Margin = new System.Windows.Forms.Padding(4);
+			this.DisplayButton.Name = "DisplayButton";
+			this.DisplayButton.Size = new System.Drawing.Size(288, 26);
+			this.DisplayButton.TabIndex = 3;
+			this.DisplayButton.Text = "Display Inventory";
+			this.DisplayButton.UseVisualStyleBackColor = true;
+			// 
+			// UpdateButton
+			// 
+			this.UpdateButton.Location = new System.Drawing.Point(19, 109);
+			this.UpdateButton.Margin = new System.Windows.Forms.Padding(4);
+			this.UpdateButton.Name = "UpdateButton";
+			this.UpdateButton.Size = new System.Drawing.Size(288, 26);
+			this.UpdateButton.TabIndex = 2;
+			this.UpdateButton.Text = "Update";
+			this.UpdateButton.UseVisualStyleBackColor = true;
+			// 
+			// RemoveButton
+			// 
+			this.RemoveButton.Location = new System.Drawing.Point(19, 77);
+			this.RemoveButton.Margin = new System.Windows.Forms.Padding(4);
+			this.RemoveButton.Name = "RemoveButton";
+			this.RemoveButton.Size = new System.Drawing.Size(288, 26);
+			this.RemoveButton.TabIndex = 1;
+			this.RemoveButton.Text = "Remove";
+			this.RemoveButton.UseVisualStyleBackColor = true;
+			// 
+			// AddButton
+			// 
+			this.AddButton.Location = new System.Drawing.Point(19, 44);
+			this.AddButton.Margin = new System.Windows.Forms.Padding(4);
+			this.AddButton.Name = "AddButton";
+			this.AddButton.Size = new System.Drawing.Size(288, 26);
+			this.AddButton.TabIndex = 0;
+			this.AddButton.Text = "Add";
+			this.AddButton.UseVisualStyleBackColor = true;
+			// 
 			// panel1
 			// 
+			this.panel1.Controls.Add(this.groupBox2);
 			this.panel1.Controls.Add(this.groupBox1);
 			this.panel1.Location = new System.Drawing.Point(1, 1);
-			this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.panel1.Margin = new System.Windows.Forms.Padding(4);
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(389, 655);
+			this.panel1.Size = new System.Drawing.Size(354, 589);
 			this.panel1.TabIndex = 0;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.ItemQuantityLabel);
+			this.groupBox2.Controls.Add(this.ItemPriceLabel);
+			this.groupBox2.Controls.Add(this.ItemNameLabel);
+			this.groupBox2.Controls.Add(this.ItemQuantity);
+			this.groupBox2.Controls.Add(this.ItemPrice);
+			this.groupBox2.Controls.Add(this.ItemName);
+			this.groupBox2.Location = new System.Drawing.Point(11, 12);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(329, 306);
+			this.groupBox2.TabIndex = 1;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Inventory Item Fields";
+			// 
+			// ItemName
+			// 
+			this.ItemName.Location = new System.Drawing.Point(19, 84);
+			this.ItemName.Name = "ItemName";
+			this.ItemName.Size = new System.Drawing.Size(288, 24);
+			this.ItemName.TabIndex = 4;
+			// 
+			// ItemPrice
+			// 
+			this.ItemPrice.Location = new System.Drawing.Point(20, 156);
+			this.ItemPrice.Name = "ItemPrice";
+			this.ItemPrice.Size = new System.Drawing.Size(288, 24);
+			this.ItemPrice.TabIndex = 5;
+			// 
+			// ItemQuantity
+			// 
+			this.ItemQuantity.Location = new System.Drawing.Point(20, 228);
+			this.ItemQuantity.Name = "ItemQuantity";
+			this.ItemQuantity.Size = new System.Drawing.Size(288, 24);
+			this.ItemQuantity.TabIndex = 6;
+			// 
+			// ItemNameLabel
+			// 
+			this.ItemNameLabel.AutoSize = true;
+			this.ItemNameLabel.ForeColor = System.Drawing.SystemColors.Control;
+			this.ItemNameLabel.Location = new System.Drawing.Point(17, 63);
+			this.ItemNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.ItemNameLabel.Name = "ItemNameLabel";
+			this.ItemNameLabel.Size = new System.Drawing.Size(94, 18);
+			this.ItemNameLabel.TabIndex = 5;
+			this.ItemNameLabel.Text = "Item Name:";
+			// 
+			// ItemPriceLabel
+			// 
+			this.ItemPriceLabel.AutoSize = true;
+			this.ItemPriceLabel.ForeColor = System.Drawing.SystemColors.Control;
+			this.ItemPriceLabel.Location = new System.Drawing.Point(17, 135);
+			this.ItemPriceLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.ItemPriceLabel.Name = "ItemPriceLabel";
+			this.ItemPriceLabel.Size = new System.Drawing.Size(89, 18);
+			this.ItemPriceLabel.TabIndex = 7;
+			this.ItemPriceLabel.Text = "Item Price:";
+			// 
+			// ItemQuantityLabel
+			// 
+			this.ItemQuantityLabel.AutoSize = true;
+			this.ItemQuantityLabel.ForeColor = System.Drawing.SystemColors.Control;
+			this.ItemQuantityLabel.Location = new System.Drawing.Point(17, 207);
+			this.ItemQuantityLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.ItemQuantityLabel.Name = "ItemQuantityLabel";
+			this.ItemQuantityLabel.Size = new System.Drawing.Size(112, 18);
+			this.ItemQuantityLabel.TabIndex = 8;
+			this.ItemQuantityLabel.Text = "Item Quantity:";
 			// 
 			// MainScreen
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.DarkSlateGray;
-			this.ClientSize = new System.Drawing.Size(1404, 655);
-			this.Controls.Add(this.panel2);
+			this.BackColor = System.Drawing.Color.DarkSlateBlue;
+			this.ClientSize = new System.Drawing.Size(1276, 589);
+			this.Controls.Add(this.UserInfoPanel);
 			this.Controls.Add(this.panel1);
-			this.Font = new System.Drawing.Font("Modern No. 20", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Margin = new System.Windows.Forms.Padding(4);
 			this.Name = "MainScreen";
 			this.Text = "Inventory Manager";
-			this.panel2.ResumeLayout(false);
-			this.panel2.PerformLayout();
-			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.UserInfoPanel.ResumeLayout(false);
+			this.UserInfoPanel.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.UserImage)).EndInit();
 			this.groupBox1.ResumeLayout(false);
 			this.groupBox1.PerformLayout();
 			this.panel1.ResumeLayout(false);
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
 
 		#endregion
-		private System.Windows.Forms.Panel panel2;
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel UserInfoPanel;
+		private System.Windows.Forms.Label UsernameLabel;
+		private System.Windows.Forms.PictureBox UserImage;
 		private System.Windows.Forms.GroupBox groupBox1;
 		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.Button button6;
-		private System.Windows.Forms.Button button5;
-		private System.Windows.Forms.Button button4;
-		private System.Windows.Forms.Button button3;
-		private System.Windows.Forms.Button button2;
-		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button SearchButton;
+		private System.Windows.Forms.Button DisplayButton;
+		private System.Windows.Forms.Button UpdateButton;
+		private System.Windows.Forms.Button RemoveButton;
+		private System.Windows.Forms.Button AddButton;
 		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Label ItemNameLabel;
+		private System.Windows.Forms.TextBox ItemQuantity;
+		private System.Windows.Forms.TextBox ItemPrice;
+		private System.Windows.Forms.TextBox ItemName;
+		private System.Windows.Forms.Label ItemQuantityLabel;
+		private System.Windows.Forms.Label ItemPriceLabel;
 	}
 }
 
